980a92e87f6faedbefb14452c6dc06810b21989f
Paramita Banerjee <paramita.banerjee@walmartlabs.com>
2018-10-30 01:50:22 -0700

    stripe APIs

--- src/main/java/food/togo/customer/service/PaymentService.java
+++ src/main/java/food/togo/customer/service/PaymentService.java
@@ -6,18 +6,21 @@ import java.util.Map;
 import com.stripe.exception.*;
 import com.stripe.model.Customer;
 import food.togo.customer.mysqldao.entities.CustomerEntity;
+import food.togo.customer.request.ChargeRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.stripe.Stripe;
 import com.stripe.model.Charge;
 
 @Service("payment")
-public class PaymentServiceImpl  {
+public class PaymentService {
 
-    private static final String TEST_STRIPE_SECRET_KEY = "sk_test_XXXXX";
+    //@Value("${STRIPE_PUBLIC_KEY}")
+    private String stripePublicKey = "pk_test_JMlbR4PSbP5qPVON78QsDW8b";
 
-    public PaymentServiceImpl() {
-        Stripe.apiKey = TEST_STRIPE_SECRET_KEY;
+    public PaymentService() {
+        Stripe.apiKey = stripePublicKey;
     }
 
     public String createStripeCustomer(CustomerEntity user) {
@@ -53,21 +56,18 @@ public class PaymentServiceImpl  {
         return id;
     }
 
-    public void chargeCreditCard(float dollarAmtInOrder, String stripeCustomerId) {
-
-        // Stripe requires the charge amount to be in cents
-        int chargeAmountCents = (int) dollarAmtInOrder * 100;
-
-        Map<String, Object> chargeParams = new HashMap<String, Object>();
-        chargeParams.put("amount", chargeAmountCents);
-        chargeParams.put("currency", "usd");
-        chargeParams.put("description", "Monthly Charges");
-        chargeParams.put("customer", stripeCustomerId);
-
+    public Charge chargeUsingTokenForFirstTimePayment(ChargeRequest chargeRequest)
+            throws AuthenticationException, InvalidRequestException, ApiConnectionException, CardException, ApiException {
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", chargeRequest.getAmount());
+        chargeParams.put("currency", chargeRequest.getCurrency());
+        chargeParams.put("description", chargeRequest.getDescription());
+        chargeParams.put("source", chargeRequest.getStripeToken());
         try {
             // Submit charge to credit card
             Charge charge = Charge.create(chargeParams);
             System.out.println(charge);
+            return charge;
         } catch (CardException e) {
             // Transaction was declined
             System.out.println("Status is: " + e.getCode());
@@ -85,5 +85,41 @@ public class PaymentServiceImpl  {
         } catch (Exception e) {
             // Something else happened unrelated to Stripe
         }
+        return null;
+    }
+
+
+    public Charge chargeCreditCardForSavedStripeCustomer(ChargeRequest chargeRequest, String stripeCustomerId) {
+
+
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", chargeRequest.getAmount());
+        chargeParams.put("currency", chargeRequest.getCurrency());
+        chargeParams.put("description", chargeRequest.getDescription());
+        chargeParams.put("customer", stripeCustomerId);
+
+        try {
+            // Submit charge to credit card
+            Charge charge = Charge.create(chargeParams);
+            System.out.println(charge);
+            return charge;
+        } catch (CardException e) {
+            // Transaction was declined
+            System.out.println("Status is: " + e.getCode());
+            System.out.println("Message is: " + e.getMessage());
+        } catch (RateLimitException e) {
+            // Too many requests made to the API too quickly
+        } catch (InvalidRequestException e) {
+            // Invalid parameters were supplied to Stripe's API
+        } catch (AuthenticationException e) {
+            // Authentication with Stripe's API failed (wrong API key?)
+        } catch (ApiConnectionException e) {
+            // Network communication with Stripe failed
+        } catch (StripeException e) {
+            // Generic error
+        } catch (Exception e) {
+            // Something else happened unrelated to Stripe
+        }
+        return null;
     }
 }
